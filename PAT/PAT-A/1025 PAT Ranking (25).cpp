fbd7738c09d906a71bce8ff3609a1bd11d10f8ec
Emerson <32270702+UNICKCHENG@users.noreply.github.com>
2018-08-08 01:15:53 -0400

    August 8,2018

--- PAT/PAT-A/1025 PAT Ranking (25).cpp
+++ PAT/PAT-A/1025 PAT Ranking (25).cpp
@@ -1,3 +1,69 @@
+/**
+ * 1025 PAT Ranking (25)
+ * https://pintia.cn/problem-sets/994805342720868352/problems/994805474338127872
+ * https://www.nowcoder.com/pat/1/problem/4004
+ */
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+//zhicheng
+//August 8,2018
+typedef struct{string num;int sco;int fin_rank;int flg;int loc_rank;}D;
+vector<D> re;
+vector<D> te; 
+bool comp(D a,D b)
+{
+	if(a.sco!=b.sco) return a.sco>b.sco;
+	else return a.num<b.num;
+}
+inline void _sort(vector<D>& a,int fg)
+{
+	sort(a.begin(),a.end(),comp);
+	if(!fg) {a[0].loc_rank=1; re.push_back(a[0]);}
+	else a[0].fin_rank=1;
+	for(int i=1;i<a.size();i++)
+	{
+		if(!fg)
+		{
+			a[i].loc_rank=(a[i].sco==a[i-1].sco)?(a[i-1].loc_rank):(i+1);
+			re.push_back(a[i]);
+		}
+		else 
+		{
+			a[i].fin_rank=(a[i].sco==a[i-1].sco)?(a[i-1].fin_rank):(i+1);
+		}
+	}
+}
+
+int main()
+{
+	freopen("1.txt","r",stdin);
+	int n,k;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++)
+	{
+		D tmp;
+		scanf("%d",&k);
+		for(int j=1;j<=k;j++)
+		{
+			cin>>tmp.num>>tmp.sco;
+			tmp.flg=i;
+			te.push_back(tmp);
+		}
+		_sort(te,0);
+		te.clear();
+	}
+	_sort(re,1);
+	cout<<re.size()<<endl;
+	for(int i=0;i<re.size();i++)cout<<re[i].num<<" "<<re[i].fin_rank<<" "<<re[i].flg<<" "<<re[i].loc_rank<<endl;
+	re.clear();
+	return 0;
+}
+
+
+------
 #include<cstdio>
 #include<iostream>
 #include<vector>
