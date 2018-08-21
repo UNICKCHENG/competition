8b70696e6f01f9cc640df79988989158b11a5ec8
Emerson <32270702+UNICKCHENG@users.noreply.github.com>
2018-08-21 17:37:30 +0800

    August 21,2018

--- PAT/PAT-A/1038 Recover the Smallest Number (30).cpp
+++ PAT/PAT-A/1038 Recover the Smallest Number (30).cpp
@@ -1,6 +1,8 @@
 /**
  * 1038 Recover the Smallest Number (30)（30 point(s)）
  * https://pintia.cn/problem-sets/994805342720868352/problems/994805449625288704
+ * 题目大意:给定一组数,自由组合,要求组成一串最小的数
+ * 解题思路:主要在于两点.其一,给这一组数进行排序;其二,组成的数字开头不可为0.
  */
 #include<cstdio>
 #include<iostream>
@@ -11,7 +13,7 @@ using namespace std;
 // August 21,2018
 const int maxn=10000+5;
 string a[maxn];
-bool com(string p,string q) {return p+q<q+p;}
+bool com(string p,string q) {return p+q<q+p;}// 给数字进行排序
 int main()
 {
 	// freopen("1.in","r",stdin);
@@ -19,7 +21,7 @@ int main()
 	for(scanf("%d",&_),n=_;_;_--) cin>>a[_];
 	sort(a+1,a+n+1,com);
 	for(int i=1;i<=n;i++)
-	{
+	{ // 组成的数字开头不可为0
 		if(flg==false)
 		{
 			int cnt=0;
