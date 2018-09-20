9922f1779687c26dd82b609ebf414e30bfa026aa
UNICKCHENG <www.20527596@outlook.com>
2018-09-20 12:59:50 +0800

    September,20,2018

--- PAT-B/牛客网PAT/1004_福尔摩斯的约会.cpp
+++ PAT-B/牛客网PAT/1004_福尔摩斯的约会.cpp
@@ -15,27 +15,25 @@ int main()
     // freopen("1.in","r",stdin);
     char s1[61],s2[61],s3[61],s4[61];int w=-1,d=-1,m=-1;
     scanf("%s\n%s\n%s\n%s",s1,s2,s3,s4);
-    for(int i=0;s1[i]!='\0'&&s2[i]!='\0';i++)
-    {// 匹配第一对字符串相关信息
-        if(s1[i]==s2[i])
-        {
+    for(int i=0;s1[i]!='\0'&&s2[i]!='\0';i++)// 匹配第一对字符串相关信息
+        if(s1[i]==s2[i]){
             if(w==-1&&'A'<=s1[i]&&'N'>=s1[i]) w=s1[i]-'A';
             else if(w!=-1&&('0'<=s1[i]&&'9'>=s1[i])||('A'<=s1[i]&&'N'>=s1[i])){
                 d=('0'<=s1[i]&&'9'>=s1[i])?s1[i]-'0':s1[i]-'A'+10;
                 break;
             }
         }
-    }
-    for(int i=0;s3[i]!='\0'&&s4[i]!='\0';i++)
-    {// 匹配第二对字符串相关信息
+    for(int i=0;s3[i]!='\0'&&s4[i]!='\0';i++)// 匹配第二对字符串相关信息
         if(s3[i]==s4[i])
             if(('A'<=s3[i]&&'Z'>=s3[i])||('a'<=s3[i]&&'z'>=s3[i])) {m=i;break;}
-    }
     printf("%s %02d:%02d\n",week[(w+(d+m/60)/24)%7],(d+m/60)%24,m%60);// 虽然字符串最长不超过60位,但仍需考虑进位问题
     return 0;
 }
 
+
+
 // --- the older code
+// It will be accepted by the data of nowcoder,but not so in PTA.
 #include<cstdio>
 #include<iostream>
 #include<algorithm>
